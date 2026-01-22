774ea3a7df5a8e697531a45ac13e696f96fd8820
deeep8250 <deepmondal825005@gmail.com>
2026-01-22 20:22:29 +0530

    added initial db connection

--- database/connection.go
+++ database/connection.go
@@ -1,1 +1,51 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/deeep8250/config"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var DB *pgxpool.Pool
+
+func ConnectDB(cfg config.Config) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		cfg.DbUser,
+		cfg.DbPassword,
+		cfg.DbHost,
+		cfg.DbPort,
+		cfg.DbName,
+	)
+
+	poolConfig, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		log.Fatal("failed to parse DB config", err.Error())
+	}
+
+	poolConfig.MaxConns = 10
+	poolConfig.MinConns = 2
+	poolConfig.MaxConnLifetime = time.Hour
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	db, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	if err != nil {
+		log.Fatal("unable to create db pool : ", err)
+	}
+
+	err = db.Ping(ctx)
+	if err != nil {
+		log.Fatal("database not reachable", err)
+	}
+
+	DB = db
+
+	log.Println("database connected successfully ")
+
+}
