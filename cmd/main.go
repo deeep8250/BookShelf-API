ab6cf5b481f05be40925dc13892badd3bf653e04
deeep8250 <deepmondal825005@gmail.com>
2026-01-22 19:12:06 +0530

    initial setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+
+	"github.com/deeep8250/config"
+	"github.com/deeep8250/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	cfg := config.Load()
+
+	router := gin.Default()
+
+	routes.RegisterRoutes(router)
+
+	log.Println("server running on port : ", cfg.Port)
+	err := router.Run(":" + cfg.Port)
+	if err != nil {
+		log.Fatal("Failed to connect with the server : ", err)
+	}
+
+}
---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string
+}
+
+func Load() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	return Config{
+		Port: port,
+	}
+
+}
