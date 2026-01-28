77ca88420f2d48c240b70df1a6eacf00ca68540e
deeep8250 <deepmondal825005@gmail.com>
2026-01-28 15:14:23 +0530

    added middleware

--- auth/middleware.go
+++ auth/middleware.go
@@ -1,1 +1,46 @@
 package auth
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "authorization header missing",
+			})
+			c.Abort()
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid authorization format",
+			})
+			c.Abort()
+			return
+		}
+
+		tokenString := parts[1]
+		userID, err := ValidateToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid or expired token",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("user_id", userID)
+		c.Next()
+
+	}
+}
